db059dee73908cee56bfd5628e85c696e7a12231
mino0123 <minoaw@gmail.com>
2013-06-14 13:21:24 +0900

    1

---
+++ test/beep.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+
+var squareModule = require('../');
+
+describe('integers', function () {
+    it('should square the numbers', function (done) {
+        assert.equal(squareModule(2), 4);
+        assert.equal(squareModule(3), 9);
+        
+        done();
+    });
+});
