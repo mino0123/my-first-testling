d69df67c949f9ae17081dbc2108bb1e3cc045ea3
mino0123 <minoaw@gmail.com>
2014-03-14 15:17:26 +0900

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,14 +1,4 @@
 var assert = require('assert');
-var global = this;
-describe('named function expression', function () {
-    it('should not leak outer scope', function (done) {
-        (function () {
-            var fn = function func(){};
-        }());
-        assert.equal(typeof func, 'undefined');
-        done();
-    });
-});
 
 describe('input要素にサポートされていない値を入力', function () {
     it('input.typeが"text"を返す', function (done) {
