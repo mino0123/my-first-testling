af18081cb222572b29cd4fbd8762b0c4ce465d61
mino0123 <minoaw@gmail.com>
2014-03-14 15:08:13 +0900

    Update test.js

--- test/test.js
+++ test/test.js
@@ -9,3 +9,11 @@ describe('named function expression', function () {
         done();
     });
 });
+
+describe('input要素にサポートされていない値を入力', function () {
+    it('input.typeが"text"を返す', function (done) {
+        var input = document.createElement('input');
+        input.type = 'InvalidType';
+        assert.equal(input, 'text');
+    });
+});
