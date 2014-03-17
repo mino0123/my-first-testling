6add7fabfad3b00799e33b699750b077bc718424
mino0123 <minoaw@gmail.com>
2014-03-17 17:33:49 +0900

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,7 +3,9 @@ var assert = require('assert');
 describe('input要素にサポートされていない値を入力', function () {
     it('input.typeが"text"を返す', function (done) {
         var input = document.createElement('input');
-        input.type = 'InvalidType';
+        try {
+            input.type = 'InvalidType';
+        } catch (e) {}
         assert.equal(input.type, 'text');
         done();
     });
